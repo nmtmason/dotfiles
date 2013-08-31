541a06cd2538186ff81e3abeb1c3b98483318d48
Nicholas Mason <nmt.mason@googlemail.com>
2013-08-31 10:37:38 +0100

    Don't copy .gitmodules to $HOME

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ git submodule init
 git submodule update
 
 for file in .*; do
-  if [ "$file" != "." ] && [ "$file" != ".." ] && [ "$file" != ".git" ]; then
+  if [ "$file" != "." ] && [ "$file" != ".." ] && [ "$file" != ".git" ] && [ "$file" != ".gitmodules" ]; then
     echo "Copying $file"
     rm -rf "$HOME/$file"
     cp -r "$file" "$HOME/$file"
